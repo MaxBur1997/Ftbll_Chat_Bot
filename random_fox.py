7a9caba15c45e617492167ae03f231a43c78ff2b
Max <burikovmaks@mail.ru>
2024-07-20 10:50:45 +0300

    Error

--- random_fox.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def fox():
-    url = 'https://randomfox.ca/floof/'
-    response = requests.get(url)
-    if response.status_code:
-        data = response.json()
-        return data.get('image')
